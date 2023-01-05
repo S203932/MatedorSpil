02d66e86dc44deff6365631a1bf6971e52d6e951
Daniel <s211449@student.dtu.dk>
2023-01-05 14:18:40 +0100

    Added a get and set for taxClaim

--- src/main/java/Fields/Tax.java
+++ src/main/java/Fields/Tax.java
@@ -2,4 +2,13 @@ package Fields;
 
 public class Tax extends Field{
     private int taxClaim;
+
+    public void setTaxClaim(int taxClaim) {
+        this.taxClaim = taxClaim;
+    }
+
+    public int getTaxClaim() {
+        return taxClaim;
+
+    }
 }
