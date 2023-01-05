6a5821562d7be5b51e54136b1acb5d80015ad98b
misaki555 <113907659+misaki555@users.noreply.github.com>
2023-01-05 14:11:07 +0100

    small changes

--- src/main/java/Fields/Tax5.java
+++ src/main/java/Fields/Tax5.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Tax5 extends Tax {
 
     public void rent(Player player, int optionTax) {
-        if (int optionTax==2){
+        if ( optionTax==2){
             player.getAccount().subtractionAccount(4000);
         } else if (optionTax == 1) {
             int percentageTax = player.getAccount().getAmount() - player.getAccount().getAmount() / 10;
