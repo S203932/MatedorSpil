52562f8e778930ba2ae41a5ba53f57984fa00f3f
Daniel <s211449@student.dtu.dk>
2023-01-05 12:53:02 +0100

    Added classes to claim Taxes on Tax fields

--- src/main/java/Fields/Tax5.java
+++ src/main/java/Fields/Tax5.java
@@ -5,7 +5,7 @@ public class Tax5 extends Tax {
 
     public void rent(Player player) {
             player.getAccount().subtractionAccount(4000);
-            int percentageTax = player.getAccount().getPengebeholdning() - player.getAccount().getPengebeholdning() / 10;
+            int percentageTax = player.getAccount().getAmount() - player.getAccount().getAmount() / 10;
 
             player.getAccount().setAccount(percentageTax);
 
