4d6caa554a0ee725ec319091e38fa67001bae20b
Daniel <s211449@student.dtu.dk>
2023-01-17 15:08:29 +0100

    JUnit tests for Player class methods

--- src/main/java/SupportClasses/PlayerTest.java
+++ src/main/java/SupportClasses/PlayerTest.java
@@ -1,10 +1,14 @@
 package SupportClasses;
 
+import Fields.Field;
+import Fields.FieldList;
+import Fields.Property;
+import Fields.RealEstate;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PlayerTest {
+class PlayerTest extends Property{
 
     @Test
     void diceRollPosition() {
@@ -14,4 +18,29 @@ class PlayerTest {
         player.diceRollPosition(5);
         assertEquals(5000, player.getAccount().getAmount());
     }
+
+    @Test
+    void eligbleForHouse(){
+        Player player = new Player();
+        FieldList fieldList = new FieldList();
+        fieldList.setupField("Dansk.txt");
+        player.setProperty((RealEstate) fieldList.getFieldIndex(1));
+        player.setProperty((RealEstate) fieldList.getFieldIndex(3));
+        player.eligbleForHouse();
+        assert(player.eligbleForHouse());
+    }
+    @Test
+    void eligbleRealEstateForHouses(){
+        Player player = new Player();
+        FieldList fieldList = new FieldList();
+
+        fieldList.setupField("Dansk.txt");
+        player.setProperty((RealEstate) fieldList.getFieldIndex(1));
+        player.setProperty((RealEstate) fieldList.getFieldIndex(3));
+        assert(player.eligbleForHouse());
+        Property[] property = player.eligbleRealEstateForHouses();
+        assertEquals("Rødovrevej", property[0].getName());
+        assertEquals("Hvidovrevej", property[1].getName());
+    }
+
 }
